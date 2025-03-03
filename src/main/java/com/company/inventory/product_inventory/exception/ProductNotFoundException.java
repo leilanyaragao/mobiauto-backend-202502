6e736cb5c425dc37110c10324f8e763e42a57125
leilanyaragao <leilanyaragao26@gmail.com>
2025-03-03 18:03:08 -0300

    refactor exceptions

--- src/main/java/com/company/inventory/product_inventory/exception/ProductNotFoundException.java
+++ src/main/java/com/company/inventory/product_inventory/exception/ProductNotFoundException.java
@@ -1,6 +1,6 @@
 package com.company.inventory.product_inventory.exception;
 
-public class ProductNotFoundException extends RuntimeException{
+public class ProductNotFoundException extends IllegalArgumentException {
     public ProductNotFoundException(String message) {
         super(message);
     }
--- src/main/java/com/company/inventory/product_inventory/exception/WarehouseNotFoundException.java
+++ src/main/java/com/company/inventory/product_inventory/exception/WarehouseNotFoundException.java
@@ -1,10 +1,6 @@
 package com.company.inventory.product_inventory.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class WarehouseNotFoundException extends RuntimeException {
+public class WarehouseNotFoundException extends IllegalArgumentException {
     public WarehouseNotFoundException(String message) {
         super(message);
     }
